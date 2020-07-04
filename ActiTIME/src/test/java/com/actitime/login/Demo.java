ccd4b6e21dd33754b16c78b37bbc3f87dd58f165
WINDOWS <WINDOWS@DESKTOP-KJSBVNF>
2020-07-04 08:36:14 +0530

    successfully loaded to local repository

---
+++ ActiTIME/src/test/java/com/actitime/login/Demo.java
@@ -0,0 +1,11 @@
+package com.actitime.login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+ public void run() {
+	 Reporter.log("Script is working successfull",true);
+ }
+}
